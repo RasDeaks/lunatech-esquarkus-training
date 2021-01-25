87303207607865195591868cd9c5b8cb4d29b497
dix <francois.dixmeras@lunatech.fr>
2021-01-25 09:24:00 +0100

    Initial commit

---
+++ src/main/java/fr/lunatech/elasticsearch/model/Fruit.java
@@ -0,0 +1,12 @@
+package fr.lunatech.elasticsearch.model;
+
+import lombok.Data;
+
+@Data
+public class Fruit {
+
+    private  String id;
+    private  String name;
+    private  String color;
+    private  String description;
+}
